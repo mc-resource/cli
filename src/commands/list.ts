4ee781e60052782ba269185c231a5dd2919c811e
Arman <armanasadi121@gmail.com>
2025-07-16 14:16:43 +0330

    fix(list): Fixed list command throwing error when concrete file not found

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -3,12 +3,16 @@ import { CommandDefinition } from "../types";
 import { concreteConfig } from "../main";
 
 export function listExecute() {
-    concreteConfig.loadConcreteFile();
+    try {
+      concreteConfig.loadConcreteFile();
     const installed = concreteConfig.manifest?.dependencies || [];
     console.log(`Found ${installed.length} installed resources:`);
     installed.forEach((res) => {
         console.log(`- ${res.name}@${res.version?.name}`);
     });
+    } catch (error) {
+      console.error("Failed to load concrete file, are you sure you have the concrete.json file? ".red, error);
+    }
 }
 
 export const ListCommand: CommandDefinition = {
